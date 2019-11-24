7961adde80a1c46acf4e0ec096d8ec2ea5bdd666
Mike Manzo <manzo.mike@gmail.com>
2019-11-24 12:48:16 -0500

    Initial Commit

---
+++ QJColorChooser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QJColorChooser
+//
+//  Created by Mike Manzo on 11/24/19.
+//  Copyright © 2019 Mike Manzo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
